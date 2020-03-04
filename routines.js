a42bfa66d3c237e51b5eaa62a7d18ea3debdf016
kev87ian <kelvinian87@gmail.com>
2020-03-04 12:58:36 +0300

    Finished adding media itens

--- routines.js
+++ routines.js
@@ -1,44 +1,90 @@
+// $(document).ready(function(){
+//     $("#img1").click(function() {
+// $("#text1").show();
+//     $("#img1").hide();    
+//     });
+// }
+// );
+// $(document).ready(function(){
+//     $("#text1").click(function(){
+//         $("#text1").hide();
+//         $("#img1").show()
+//     });
+
+// });
+
+// $(document).ready(function(){
+//     $("#img2").click(function() {
+// $("#text2").show();
+//     $("#img2").hide();    
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#text2").click(function(){
+//         $("#text2").hide();
+//         $("#img2").show()
+//     });
+
+// });
+
+// $(document).ready(function(){
+//     $("#img3").click(function() {
+// $("#text3").show();
+//     $("#img3").hide();    
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#text3").click(function(){
+//         $("#text3").hide();
+//         $("#img3").show()
+//     });
+
+// });
+
+
 $(document).ready(function(){
-    $("#img1").click(function() {
-$("#text1").show();
-    $("#img1").hide();    
+    $("#vid1").click(function() {
+$("#text4").show();
+    $("#vid1").hide();    
     });
 }
 );
 $(document).ready(function(){
-    $("#text1").click(function(){
-        $("#text1").hide();
-        $("#img1").show()
+    $("#vid1").click(function(){
+        $("#text4").hide();
+        $("#vid1").show()
     });
 
 });
 
 $(document).ready(function(){
-    $("#img2").click(function() {
-$("#text2").show();
-    $("#img2").hide();    
+    $("#vid2").click(function() {
+$("#text5").show();
+    $("#vid2").hide();    
     });
 });
 
 $(document).ready(function(){
-    $("#text2").click(function(){
-        $("#text2").hide();
-        $("#img2").show()
+    $("#text5").click(function(){
+        $("#text5").hide();
+        $("#vid2").show()
     });
 
 });
 
 $(document).ready(function(){
-    $("#img3").click(function() {
-$("#text3").show();
-    $("#img3").hide();    
+    $("#vid3").click(function() {
+$("#text6").show();
+    $("#vid3").hide();    
     });
 });
 
 $(document).ready(function(){
-    $("#text3").click(function(){
-        $("#text3").hide();
-        $("#img3").show()
+    $("#text6").click(function(){
+        $("#text6").hide();
+        $("#vid3").show()
     });
 
 });
