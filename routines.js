09bd38c5f8894a37855621fd6b8689245af00003
kev87ian <kelvinian87@gmail.com>
2020-03-04 10:38:59 +0300

    Add hide/show functionality

---
+++ routines.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#img1").click(function() {
+$("#text1").show();
+    $("#img1").hide();    
+    });
+}
+);
+$(document).ready(function(){
+    $("#text1").click(function(){
+        $("#text1").hide();
+        $("#img1").show()
+    });
+
+});
+
+$(document).ready(function(){
+    $("#img2").click(function() {
+$("#text2").show();
+    $("#img2").hide();    
+    });
+});
+
+$(document).ready(function(){
+    $("#text2").click(function(){
+        $("#text2").hide();
+        $("#img2").show()
+    });
+
+});
+
+$(document).ready(function(){
+    $("#img3").click(function() {
+$("#text3").show();
+    $("#img3").hide();    
+    });
+});
+
+$(document).ready(function(){
+    $("#text3").click(function(){
+        $("#text3").hide();
+        $("#img3").show()
+    });
+
+});
